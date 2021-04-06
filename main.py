c19a96fa388d340c5d0e2f8e4e15975b69eb388b
ferdinand <ferdinandmwal@gmail.com>
2021-04-06 16:06:51 +0300

    working

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ import pynput
 
 from pynput.keyboard import Key, Listener
 
-with Listener(on_press=on_press, on_relsease=on_release)
+
+def on_press(key):
+    print(key)
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press=on_press, on_relsease=on_release) as listener:
+    listener.join()
